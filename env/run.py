2b8e56ee839b3b48e9268cd3feae475143c6bc55
yevhen-tantsiura <tantsiura_69225@cloud.wsb.wroclaw.pl>
2021-04-27 18:15:33 +0200

    v0.1.1

--- env/run.py
+++ env/run.py
@@ -1,16 +1,35 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from flask import abort, redirect, url_for, make_response
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
-@app.route('/help')
-def help():
-    return "help me"
+    return render_template("index.html")
 
+@app.route("/gallery/")
+def gallery():
+    return render_template ("gallery.html")
 
-    app.run()
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route('/error_denied')
+def error_denied():
+    abort(401)
+@app.route('/error_internal')
+def error_internal():
+    abort(505)
+@app.route('/error_not_found')
+def error_not_found():
+    abort(404)
+
+if __name__ == '__main__':
+ app.run(debug=True)
