3a910615e12949b8fa130d65129b2004de67f081
yevhen-tantsiura <tantsiura_69225@cloud.wsb.wroclaw.pl>
2021-04-27 15:44:49 +0200

    first commit

---
+++ env/run.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import abort, redirect, url_for, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/help')
+def help():
+    return "help me"
+
+
+if __name__ == '__main__':
+    app.run()
